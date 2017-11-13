7cebaad940682a653c2a3191fbc86d5b9ee2abf4
alextricity25 <miguel.can2@gmail.com>
2017-11-13 15:02:06 -0800

    Fixed a few things

--- ftta_groupme_bots/settings.py
+++ ftta_groupme_bots/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ftta_groupme_bots',
+    'data',
     'djcelery',
     'kombu.transport.django'
 )
