0ba2cfa288e1feca83e3f58f12b516fd64f34b78
alextricity25 <miguel.can2@gmail.com>
2017-11-22 14:00:39 -0800

    Added ability to reference hymnal
    Users can now reference any number from the guitar hymnal

--- data/views.py
+++ data/views.py
@@ -6,6 +6,7 @@ from .models import Greeting
 
 import time
 import requests
+import re
 import random
 import os
 import logging
@@ -21,15 +22,25 @@ def ride(request):
     some_ride_data = "The time is: {}".format(time.ctime())
     return render(request, 'ride.html', {'time': some_ride_data})
 
+def _upload_groupme_image(access_token, filename):
+    url = 'https://image.groupme.com/pictures'
+    headers = {
+        'X-Access-Token': access_token,
+        'Content-Type': 'image/png'
+    }
+    r = requests.post(
+        url,
+        data=open("{}.png".format(filename), 'rb').read(),
+        headers=headers
+    )
+    return r
+
 @csrf_exempt
 def pick_a_song(request):
     logger = logging.getLogger(__name__)
     if request.method == 'POST':
-        # MemoryVerseBot bot_id
-        ACCESS_TOKEN = 'INSERTTOKEN'
         BOT_ID = os.environ['GROUPME_BOT_ID']
-        # SingSongsToHim bot_id
-        #ACCESS_TOKEN = 'INSERTOKEN'
+        ACCESS_TOKEN = os.environ['GROUPME_ACCESS_TOKEN']
         # URL of bot post
         url = "https://api.groupme.com/v3/bots/post"
         # Headers for the bot to post
@@ -44,20 +55,43 @@ def pick_a_song(request):
             }
             r = requests.post(url, json=data)
 
-        if '#sendsong' in r_dict['text'].lower():
+        if ('#sendsong' in r_dict['text'].lower() or
+            '#hymn' in r_dict['text'].lower()):
             from wand.image import Image
             import PyPDF2
+
+            # The user can either get a random song from
+            # the uk yp songbook, or requests a specific
+            # hymn number from the guitar hymnal
+            if '#hymn' in r_dict['text'].lower():
+                songbook_name = "guitar_hymnal.pdf"
+                hymn_rt = re.search(r"#hymn ([0-9]+)", r_dict['text'])
+                if hymn_rt:
+                    page_number = int(hymn_rt.group(0))
+                else:
+                    # If page number is not recognized by the regexp above,
+                    # then always return page number 1
+                    page_number = 1
+            elif '#sendsong' in r_dict['text'].lower():
+                songbook_name = "uk_songbook.pdf"
+            
+
             # Getting random page from the UK songbook...
-            pdf_file_obj = open('/app/data/uk_songbook.pdf', 'rb')
+            #pdf_file_obj = open('/app/data/uk_songbook.pdf', 'rb')
+            pdf_file_obj = open("/app/data/{}".format(songbook_name), 'rb')
             pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj, strict=False)
             pdf_writer = PyPDF2.PdfFileWriter()
             number_of_pages = pdf_reader.numPages
-            random_page = random.randint(0, number_of_pages -1)
-            page_obj = pdf_reader.getPage(random_page)
+            # If a song from the uk yp songbook is requested, then pick a random
+            # page.
+            if '#sendsong' in r_dict['text'].lower():
+                page_number = random.randint(0, number_of_pages -1)
+
+            page_obj = pdf_reader.getPage(page_number)
             pdf_writer.addPage(page_obj)
 
             # Save random page as seperate file
-            filename = "/tmp/uk_songbook_{}".format(random_page)
+            filename = "/tmp/songbook_{}".format(random_page)
             pdf_output = open(filename, 'wb')
             pdf_writer.write(pdf_output)
             pdf_output.close()
@@ -68,13 +102,7 @@ def pick_a_song(request):
                 img.save(filename="{}.png".format(filename))
 
             # Send to GroupMe Image Service
-            ACCESS_TOKEN = os.environ['GROUPME_ACCESS_TOKEN']
-            image_api_url = 'https://image.groupme.com/pictures'
-            image_api_headers = {
-                'X-Access-Token': ACCESS_TOKEN,
-                'Content-Type': "image/png"
-            }
-            r = requests.post(image_api_url, data=open("{}.png".format(filename), 'rb').read(), headers=image_api_headers)
+            r = _upload_groupme_image(ACCESS_TOKEN, filename)
             logger.info("ALEX - Image service response: {}".format(r.text))
             response_dict = json.loads(r.text)
             image_url = response_dict['payload']['picture_url']
