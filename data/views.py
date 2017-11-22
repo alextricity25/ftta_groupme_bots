a88615528baa28635712e56962d7258eb7bec110
alextricity25 <miguel.can2@gmail.com>
2017-11-22 14:14:45 -0800

    revert negate changes

--- data/views.py
+++ data/views.py
@@ -98,10 +98,7 @@ def pick_a_song(request):
 
             # Convert pdf document to PNG
             with Image(filename=filename, resolution=200) as img:
-                # Negate the image if it's from the UK YP songbook.
-                # This is to help with visuals
-                if '#sendsong' in r_dict['text']:
-                    img.negate(grayscale=True)
+                img.negate(grayscale=True)
                 img.save(filename="{}.png".format(filename))
 
             # Send to GroupMe Image Service
