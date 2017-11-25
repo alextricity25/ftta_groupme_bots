75476bb51359e7b36495a210f5a1c58e733a673a
alextricity25 <miguel.can2@gmail.com>
2017-11-25 14:18:56 -0800

    Added MemoryVerse model
    This Model represents a memory verse from a list of predefined
    classes.

--- data/models.py
+++ data/models.py
@@ -4,5 +4,24 @@ from django.db import models
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
 
+class MemoryVerse(models.Model):
+
+    CLASSES = (
+        ('ECAL', 'Experience of Christ as Life'),
+        ('GE', "God's Economy"),
+        ('GoW', "God Ordained Way")
+    )
+    ftta_term = models.CharField(
+        max_length=255,
+    )
+    reference = models.CharField(
+        max_length=100,
+    )
+    verse = models.TextField()
+    for_class = models.CharField(max_length=100, choices=CLASSES)
+
+    def __str__(self):
+        return self.reference
+
 class RideArranger(models.Model):
     pass
